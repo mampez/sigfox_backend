3c39543d82bc51b0db7bf29c1bc33bf55660a060
menchopez <mmp23uc@gmail.com>
2017-11-07 16:20:25 +0100

    Update sigfox_backend.py

--- modules/sigfox_backend.py
+++ modules/sigfox_backend.py
@@ -1,312 +1,269 @@
-"""
+""" 
 ----------------------------------------
+  - Based on pysigfox.py by Hecko
+
   - Developed by Mencho: 08/02/2017
 
   - Last Update: 09/10/2017
 ----------------------------------------
+- Classes
+    -sigfox
+- Methods
+    - device_types_list
+    - device_list_id
+    - device_all_messages
+    - device_n_messages
+    - device_all_messages_time
 
-- Functions
-  - get_links_filter
-  - get_links_all
-  - encode_ascii
-  - read_links
-  - get_rss
-  - download_file
-  - podcast_download
-  - mp3_tagging
-   -write_file
 """
-import os
-import csv
-import datetime
-import warnings
-import unicodedata
 import socket
-from dateutil import parser
-import feedparser
+import json 
+import ast
+import time
 import requests
 import requests.packages.urllib3
-from mutagen.easyid3 import EasyID3
-import mutagen
 
+## Request timeout
+TIMEOUT = 30
 
-## Timeout Requests
-TIMEOUT = 120
+class Sigfox(object):
+    """Sigfox class"""
 
+    def __init__(self, login, password):
+        if not login or not password:
+            raise Exception("Login/Pass is needed it")
+        self.login = login
+        self.password = password
+        self.api_url = 'https://backend.sigfox.com/api/'
 
-def get_links_filter(rss, keyword, number_links):
-    """Generate podcast_list with a number_links
-       for download in based on a specific keyword 
-       and rss list. 
+    def login_test(self):
+        """Test sigfox Backend login
 
-       keyword arguments:
-       rss                   --  list with rss elements
-       keyword               --  Filter links by keyword 
-       number_links          --  Number of links
+            keyword arguments:
+            None          
 
-       Return arguments:
-       podcast_list          --  list with 'podcast_data' elements
-
-       Each element of the list contains a unique podcast_data:
-
-       entry.published = u'Thu, 3 Aug 2017 11:09:51 +0100' --> '2017-08-03'
-       publishedDate = str(parser.parse(entry.published))[:10]
-
-       podcast_data = [publishedDate, 
-                      entry.enclosures[0]['href'], 
-                      rss.feed.title]
-    
-       podcast_data = [u'Thu, 3 Aug 2017 11:09:51 +0100', 
-                       u'Tertulia de Federico: M\xe1s ataques de las CUP contra el turismo', 
-                       u'http://audios.esradio.fm/espana/17/08/03/
-                         tertulia-de-federico-mas-ataques-de-las-cup-contra-el-turismo-116287.mp3', 
-                       u'Es la Ma\xf1ana de Federico']
-    """
-    podcast_list = []
-    podcast_data = []
-
-    for entry in rss.entries:
-        if keyword in entry.title:          
-            try:
-                podcast_data = [entry.published, entry.title, 
-                                entry.enclosures[0]['href'], 
-                                rss.feed.title]
-            except IOError as error:
-                print 'Error' + (error) + ': File - ' + str(entry.title)
-            podcast_list.append(podcast_data)
-            if number_links != 0:
-                if len(podcast_list) == number_links: 
-                    return podcast_list
-    return podcast_list
-
-
-def get_links_all(rss, number_links):
-    """Generate podcast_list with a number_links
-       for download in based on a rss list. 
-
-       keyword arguments:
-       rss                   --  list with rss elements 
-       number_links          --  Number of links
-
-       Return arguments:
-       podcast_list          --  list with 'podcast_data' elements
-
-       Each element of the list contains 
-       a unique podcast_data:
-
-       entry.published = u'Thu, 3 Aug 2017 11:09:51 +0100' --> '2017-08-03'
-
-       podcast_data = [publishedDate, 
-                      entry.enclosures[0]['href'], 
-                      rss.feed.title]
-    
-       podcast_data = [u'Thu, 3 Aug 2017 11:09:51 +0100', 
-                       u'Tertulia de Federico: M\xe1s ataques de las CUP contra el turismo', 
-                       u'http://audios.esradio.fm/espana/17/08/03/
-                         tertulia-de-federico-mas-ataques-de-las-cup-contra-el-turismo-116287.mp3', 
-                       u'Es la Ma\xf1ana de Federico']
-    """
-    podcast_list = []
-    podcast_data = []
-
-    for entry in rss.entries:
+            Return arguments:
+            text message --> 'Login Complete'
+        """
+        url = self.api_url + 'devicetypes'
         try:
-            podcast_data = [entry.published, entry.title, 
-                            entry.enclosures[0]['href'], 
-                            rss.feed.title]
-        except IOError as error:
-            print 'Error' + (error) + ': File - ' + str(entry.title)
-        podcast_list.append(podcast_data)
-        if number_links != 0:
-            if len(podcast_list) == number_links: 
-                return podcast_list
-    return podcast_list
-
-
-def encode_ascii(input_str):
-    """Encode text to avoid accents and special chars (ASCII)
-        
-       keyword arguments:
-       input_str             --  Input string
-
-       Return arguments:
-       Encode ASCII string
-    """
-    line_unicode = unicode(input_str, "utf-8")
-    return unicodedata.normalize('NFKD', line_unicode).encode('ASCII', 'ignore')
-
-
-def read_links(foldername, filename):
-    """Return list with the RSS link save in csv file
-
-       keyword arguments:
-       foldername             --  CSV folder
-       filename               --  CSV filename 
-
-       Return arguments:
-       List with rss links
-
-       rss_links: ['http://esradio.libertaddigital.com/es-la-manana-de-federico/podcast.xml', 
-                   'http://www.ondacero.es/rss/podcast/8272/podcast.xml', 
-                   'http://feeds.kexp.org/kexp/songoftheday']
-    """
-    rss_links = []
-    file_input = filename
-
-    if foldername:
-        file_input = os.path.join(foldername, file_input)
-    try:
-        with open(file_input, 'rb') as csvfile:
-            file_read = csv.reader(csvfile, delimiter=';', quotechar='|')
-            for row in file_read:
-                rss_links.append(row[1])
-    except IOError as error:
-        print error
-    return rss_links
-
-
-def get_rss(rss_name):
-    """Donwload and parse rss file
-
-       keyword arguments:
-       rss_name               --  RSS name
-
-       Return arguments:
-       RSS list 
-    """
-    rssfiles = []
-    now = datetime.datetime.now() 
-    for link in read_links('Links', rss_name):
-        rssfiles.append(feedparser.parse(link))
-    end = datetime.datetime.now()
-    print'RSS download Time = ' + str(end-now) + '\n'
-    return rssfiles
-
-
-def download_file(url, outputfile):
-    """Download file (GET URL)
-
-       keyword arguments:
-       url               --  podcast url
-       outputfile        --  output file
-
-       Return arguments:
-       None
-    """
-    try:
-        req = requests.get(url, stream=True, timeout=TIMEOUT)
-        try:
-            with open(outputfile, 'wb') as file_download:
-                for chunk in req.iter_content(chunk_size=1024): 
-                    if chunk: 
-                        file_download.write(chunk)
-        except IOError as error:
-            print error
-    except requests.exceptions.RequestException as err:
-        print err
-    except socket.error as err:
-        print err
-    return None
-
-
-def podcast_download(podcast_list):
-    """Manage the podcast donwload, record in a 
-       log file and convert the file to a tagged mp3
-
-       keyword arguments:
-       podcast_list      --  podcast data
-
-       Return arguments:
-       None
-
-       podcast_data = [u'Thu, 3 Aug 2017 11:09:51 +0100', 
-                       u'Tertulia de Federico: M\xe1s ataques de las CUP contra el turismo', 
-                       u'http://audios.esradio.fm/espana/17/08/03/
-                         tertulia-de-federico-mas-ataques-de-las-cup-contra-el-turismo-116287.mp3', 
-                       u'Es la Ma\xf1ana de Federico']
-    """
-    warnings.filterwarnings("ignore", category=UnicodeWarning)   
-    published, name, link, title = podcast_list
-    now = datetime.datetime.now()
-    if not os.path.exists('downloads'):
-        os.makedirs('downloads')
-    if not os.path.exists('logs'):
-        os.makedirs('logs')
-    download_log = os.path.join('logs', 'log_download.csv')
-    if not os.path.exists(download_log):    
-        file(download_log, 'w').close()
-        line_file = str('podcast_date' + ';' + 'podcast_title' + 'Name' + ';' + 'Link' + ';')
-        write_file(download_log, line_file.encode("UTF-8"))     
-    if podcast_list != []:
-        line_file = (published + ';' + title + ';' + name + ';' + link).encode("utf-8") 
-        if line_file in open(download_log).read():
-            pass
+            req = requests.get(url, auth=requests.auth.HTTPBasicAuth(self.login, self.password))
+            req.raise_for_status()
+            print "Login Complete\n"
+        except requests.exceptions.RequestException as err:
+            print err
+        except socket.error as err:
+            print err
         else:
-            title = unicodedata.normalize('NFKD', title).encode('ASCII', 'ignore')
-            download_folder = os.path.join('downloads', title)
-            if not os.path.exists(download_folder): 
-                os.makedirs(download_folder)
+            pass
+
+
+    def device_types_list(self):
+        """Return device type list
+
+            Keyword arguments:
+            None          
+
+            Return arguments:
+            List with all the device types 
+        """
+        out = []
+
+        url = self.api_url + 'devicetypes'
+        try:
+            req = requests.get(url, auth=requests.auth.HTTPBasicAuth(self.login, self.password))
+            req.raise_for_status()
+            for device in json.loads(req.text)['data']:
+                out.append(device['id'])
+            out = ast.literal_eval(json.dumps(out))
+        except requests.exceptions.RequestException  as err:
+            print err
+        except socket.error as err:
+            print err
+        else:
+            pass
+        return out       
+
+
+    def device_list_id(self, device_type_id=0):
+        """Return device list
+
+           Keyword arguments:
+           device_type_id          -- device types ID
+    
+           Return arguments:
+           List with all the PIDs 
+        """
+        device_type_ids = []
+        out = []
+        next_url = 1
+        
+        if device_type_id != 0:
+            device_type_ids.append(device_type_id)
+        else:
+            device_type_ids = self.device_types_list()
+        for device_type_id in device_type_ids:
+            url = self.api_url + 'devicetypes/' + device_type_id + '/devices?limit=100'
+            while next_url != None:
+                try:
+                    req = requests.get(url, 
+                                       auth=requests.auth.HTTPBasicAuth(self.login, self.password))
+                    req.raise_for_status()
+                    results = req.json()
+                    for result in results['data']:
+                        out.append(result['id'])
+                    out = ast.literal_eval(json.dumps(out))
+                    next_url = (results['paging']).get('next')
+                    url = next_url
+                except requests.exceptions.RequestException as err:
+                    print err
+                except socket.error as err:
+                    print err
+                else:
+                    pass
+        return out
+
+
+    def device_all_messages(self, device_id):
+        """Return a list with all sigfox messages
+           
+           Keyword arguments:
+           device_id          -- device PID
+    
+           Return arguments:
+           List with all the messages 
+
+           [pid, timestramp, snr, link_quality, data] 
+        """
+        out = []
+        next_url = 1
+
+        url = self.api_url + 'devices/' + str(device_id) + '/messages?limit=100'
+        while next_url != None: 
             try:
-                published = str(parser.parse(published))[:10]
-            except IOError as error:
-                print 'Error' + (error) + ': File - ' + str(title)
-            download_folder = os.path.join(download_folder, published)
-            if not os.path.exists(download_folder): 
-                os.makedirs(download_folder)
-            namefile_unicode = link[link.rfind('/')+1:]
-            namefile_str = unicodedata.normalize('NFKD', namefile_unicode).encode('ASCII', 'ignore')
-            namefile_str = namefile_str.decode('utf-8', 'ignore').encode("utf-8")
-            if '.mp3' in namefile_str:
-                len_name = namefile_str.index('.mp3')
-            elif '.MP3' in namefile_str:
-                len_name = namefile_str.index('.MP3')
-            namefile_str = namefile_str[:len_name + 4]
-            fileoutput = os.path.join(download_folder, namefile_str)
-            name = unicodedata.normalize('NFKD', name).encode('ASCII', 'ignore')
-            print 'Downloading: ' + name
-            print 'Date: ' + str(published)
-            download_file(link, fileoutput) 
-            mp3_tagging(fileoutput, podcast_list)
-            write_file(download_log, line_file)
-            end = datetime.datetime.now()
-            print 'Download Time = ' + str(end-now) + '\r'
-    return None
+                req = requests.get(url, 
+                                   auth=requests.auth.HTTPBasicAuth(self.login, self.password), 
+                                   timeout=TIMEOUT)
+                req.raise_for_status()
+                results = req.json()
+                results = ast.literal_eval(json.dumps(results))
+                for result in results['data']:
+                    out.append([result['device'],
+                                time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(result['time'])), 
+                                result['snr'], 
+                                result['linkQuality'], 
+                                result['data']
+                               ]
+                              )
+                next_url = (results['paging']).get('next')
+                url = next_url
+            except requests.exceptions.RequestException as err:
+                print err
+            except socket.error as err:
+                print err
+            else:
+                pass
+        return out
 
 
-def mp3_tagging(file_input, podcast_list):
-    """Tagging mp3 files
-       
-       keyword arguments:
-       file_input      --  podcast filename
-       podcast_list    --  podcast data
+    def device_n_messages(self, device_id, n_messages):
+        """Return a list up to N sigfox messages    
 
-       Return arguments:
-       None
-    """
-    try:
-        tag = EasyID3(file_input)
-    except mutagen.id3.ID3NoHeaderError:
-        tag = mutagen.File(file_input, easy=True)
-        tag.add_tags()
-    tag['album'] = podcast_list[3]
-    tag.save(v2_version=3)
-    return None
+           Keyword arguments:
+           device_id          -- device PID
+           n_messages         -- Max number of messages
+    
+           Return arguments:
+           List with the first n messages 
+
+           [pid, timestramp, snr, link_quality, data] 
+        """
+        out = []
+        num_frames = 0
+        next_url = 1
+
+        url = self.api_url + 'devices/' + str(device_id) 
+        if n_messages <= 100:
+            url = url + '/messages?limit=' + str(n_messages) 
+        else:
+            url = url + '/messages?limit=100' 
+        while (next_url != None) and (num_frames < n_messages):     
+            try:
+                req = requests.get(url, 
+                                   auth=requests.auth.HTTPBasicAuth(self.login, self.password), 
+                                   timeout=TIMEOUT)
+                req.raise_for_status()
+                results = req.json()
+                results = ast.literal_eval(json.dumps(results))
+                for result in results['data']:
+                    out.append([result['device'], 
+                                time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(result['time'])), 
+                                result['snr'], 
+                                result['linkQuality'], 
+                                result['data']
+                               ]
+                              )
+                    num_frames = num_frames + 1
+                next_url = (results['paging']).get('next') 
+                url = next_url
+            except requests.exceptions.RequestException as err:
+                print err
+            except socket.error as err:
+                print err
+            else:
+                pass
+        return out
 
 
-def write_file(file_name, line):
-    """ Writing File
+    def device_all_messages_time(self, device_id, from_time, upto_time):
+        """Return a list with all messages between from_time and upto_time
 
-        keyword arguments:
-        file_input      --  log file
-        line            --  podcast log line
+           Keyword arguments:
+           device_id          -- device PID
+           n_messages         -- Max number of messages
+           from_time          -- Start time  (format: '2017-10-01')
+           upto_time          -- Finish time (format: '2017-10-01')
+    
+           Return arguments:
+           List with n messages between from_time and upto_time
 
-        Return arguments:
-        None
-    """
-    try:
-        with open(file_name, "a") as filename:
-            filename.write(line)
-            filename.write('\n')
-    except IOError as error:
-        print error
-    return None
+           [pid, timestramp, snr, link_quality, data] 
+        """
+        out = []
+        next_url = 1
+
+        try:
+            since_epoch = int(time.mktime(time.strptime(from_time, '%Y-%m-%d')))
+            upto_epoch = int(time.mktime(time.strptime(upto_time, '%Y-%m-%d')))
+        except ValueError:
+            print "Error with the date conversion"
+        if since_epoch > upto_epoch:
+            print 'upto_time should be bigger than from_time'
+            exit()
+        url = self.api_url + 'devices/' + str(device_id) + '/messages?since=' + \
+              str(since_epoch) + '&before=' + str(upto_epoch)
+        while next_url != None:      
+            try:
+                req = requests.get(url, 
+                                   auth=requests.auth.HTTPBasicAuth(self.login, self.password), 
+                                   timeout=TIMEOUT)
+                req.raise_for_status()
+                results = req.json()
+                results = ast.literal_eval(json.dumps(results))
+                for result in results['data']:
+                    out.append([result['device'],
+                                time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(result['time'])), 
+                                result['snr'], 
+                                result['linkQuality'], 
+                                result['data']
+                               ]
+                              )
+                next_url = (results['paging']).get('next')
+                url = next_url
+            except requests.exceptions.RequestException as err:
+                print err
+            except socket.error as err:
+                print err
+            else:
+                pass
+        return out
